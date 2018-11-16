1a2b1cc45c73dfc83d4315d445a4622a578d59b2
mcannon <m.cannon.dev@gmail.com>
2018-11-16 14:35:00 +0000

    Refactor to improve naming and remove unnecessary code.

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import Store from "./store/reducer.js";
 import Safari from "./components/safari";
-import Store from "./store/read-models.js";
-
-let store = createStore(Store);
 
 ReactDOM.render(
     <Provider store={createStore(Store)}>
