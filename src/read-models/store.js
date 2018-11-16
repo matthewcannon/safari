597bd85c67ad63ee31c64869ecd4b509ba1ae40c
mcannon <m.cannon.dev@gmail.com>
2018-11-16 10:43:27 +0000

    Use the object spread operator in the read model reducer.

--- src/read-models/store.js
+++ src/read-models/store.js
@@ -6,23 +6,12 @@ export default function Store(state = InitialState, event) {
     let nextState;
 
     switch (event.type) {
-        //
-        // Shouldn't these be past tense?
-        //
-        // Then it makes sense for these handlers to be registering for events that have occured (perhaps as a result of
-        // commands being handled elsewhere), with the new state representing the eventually consistent model that is to
-        // be applied here to the read model.
-        //
         case Event.REGENERATED:
-            nextState = Object.assign({}, state, {
-                generation: state.generation + 1,
-            });
+            nextState = { ...state, generation: state.generation + 1 };
             break;
 
         case Event.STARTED:
-            nextState = Object.assign({}, state, {
-                generation: 0,
-            });
+            nextState = { ...state, generation: 0 };
             break;
 
         default:
